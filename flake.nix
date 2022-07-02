27b58f4920363e35031c6b464af56bea7bef9b08
Michal <michal@tar.black>
2022-07-02 10:15:25 +0000

    did all the shit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShell = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [ nix-linter statix nixfmt ];
+        });
+
+      homeConfigurations.michal = home-manager.lib.homeManagerConfiguration {
+          configuration = { pkgs, lib, ... }: {
+            imports = [ ./michal/shell.nix ./michal/dev.nix ./michal/base.nix ];
+          };
+        };
+      };
+}
